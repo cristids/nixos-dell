5d5d289df532441eff69c8ad6c65366936c51576
Cristian Stamateanu <cristids@gmail.com>
2025-04-07 20:14:22 +0300

    modify flake

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -15,19 +15,19 @@
         ./dell/drivers/video.nix
         ./generic/configuration.nix
 
-        # make home-manager as a module of nixos
-        # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.backupFileExtension = "hm-bkp";
+        # # make home-manager as a module of nixos
+        # # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+        # home-manager.nixosModules.home-manager
+        # {
+        #   home-manager.useGlobalPkgs = true;
+        #   home-manager.useUserPackages = true;
+        #   home-manager.backupFileExtension = "hm-bkp";
 
-          # TODO replace ryan with your own username
-          home-manager.users.cristian = import ./generic/home.nix;
+        #   # TODO replace ryan with your own username
+        #   home-manager.users.cristian = import ./generic/home.nix;
 
-          # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
-        }
+        #   # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+        # }
       ];
     };
 
