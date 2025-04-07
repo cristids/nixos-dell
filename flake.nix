738a42fce4d71364801332c289e9e22fd734641b
Cristian Stamateanu <cristids@gmail.com>
2025-04-07 19:34:33 +0300

    update flake

--- flake.nix
+++ flake.nix
@@ -5,12 +5,28 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
   };
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, home-manager, ... }: {
 
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.gpdp4-nix = nixpkgs.lib.nixosSystem {
       # NOTE: Change this to aarch64-linux if you are on ARM
       system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
+      modules = [
+        ./config.nix
+
+        # make home-manager as a module of nixos
+        # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "hm-bkp";
+
+          # TODO replace ryan with your own username
+          home-manager.users.cristian = import ./home.nix;
+
+          # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+        }
+      ];
     };
 
   };
