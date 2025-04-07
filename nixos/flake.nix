1500954dd6e0cbe24ac3f4208a2a3ae789bbbc8b
Cristian Stamateanu <cristids@gmail.com>
2025-04-07 18:59:43 +0300

    Trying another structure

---
+++ nixos/flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs, ... }: {
+
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      # NOTE: Change this to aarch64-linux if you are on ARM
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
+    };
+
+  };
+}
